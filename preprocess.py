deee8264c3a2577743401db1e677d0914bf13e62
abdullahi <abdullahi154@gmail.com>
2017-04-17 15:31:30 +0200

    first training trail

--- preprocess.py
+++ preprocess.py
@@ -273,7 +273,7 @@ csv_f.close()
 csv_f = open('/media/srv/data/preprocess/meta/train.csv', 'w')
 process_non_native(csv_f,"NorthWind")
 csv_f.close()
-"""
+
 # VCTK corpus
 csv_f = open('/media/srv/data/preprocess/meta/train.csv', 'w')
 process_vctk(csv_f)
@@ -317,4 +317,3 @@ csv_f = open('/media/srv/data/preprocess/meta/test.csv', 'a+')
 process_ted(csv_f, 'test')
 csv_f.close()
 
-"""
