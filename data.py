924df27e003c62b8a67745d3d39e24ad9a075a2a
abdullahi <abdullahi154@gmail.com>
2017-05-03 21:54:52 +0200

    implemented data enlarging

--- data.py
+++ data.py
@@ -120,8 +120,8 @@ class SpeechCorpus(object):
 
         # Artificially enlarge the data set if non-native data
         if set_name == 'non_native_train':
-            label *= 500
-            mfcc_file *= 500
+            label *= 5000
+            mfcc_file *= 5000
         # to constant tensor
         label_t = tf.convert_to_tensor(label)
         mfcc_file_t = tf.convert_to_tensor(mfcc_file)
