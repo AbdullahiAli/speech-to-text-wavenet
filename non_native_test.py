14f2d930e23840d651edd66012b379ff34f247aa
abdullahi <abdullahi154@gmail.com>
2017-05-30 19:42:29 +0200

    Strategy2

--- non_native_test.py
+++ non_native_test.py
@@ -29,9 +29,7 @@ train_labels = non_native_data_train.label
 x_train = tf.placeholder(dtype=tf.sg_floatx, shape=(batch_size, None, 20))
 train_logit = get_logit(x_train, voca_size=voca_size)
 estimator = SGDClassifier()
-print(type(np.array(train_logit)))
-print(type(train_labels))
-estimator.fit(train_logit, train_labels)
+estimator.fit(np.array(train_logit), train_labels)
 
 #
 # inputs
