579873d2f44e0d42b567e4f01b93ca59f6b4b803
abdullahi <abdullahi154@gmail.com>
2017-06-03 12:22:52 +0200

    Strategy2

--- data.py
+++ data.py
@@ -120,8 +120,7 @@ class NonNativeSpeechCorpus(object):
                       mfcc_file.append(_data_path + 'preprocess/non_native_train_mfcc/' + row[0] + '.npy')
                 elif set_name == "non_native_test":
                     mfcc_file.append(_data_path + 'preprocess/non_native_test_mfcc/' + row[0] + '.npy')
-                else:
-                    mfcc_file.append(_data_path + 'preprocess/mfcc/' + row[0] + '.npy')
+               
                 # label info ( convert to string object for variable-length support )
                 label.append(np.asarray(row[1:], dtype=np.int).tostring())
 
