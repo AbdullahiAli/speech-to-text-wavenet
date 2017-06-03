a1be8b7e8b6cb6951f70211225c56b2bcaf74e80
abdullahi <abdullahi154@gmail.com>
2017-06-03 11:59:57 +0200

    Strategy2

--- retrain.py
+++ retrain.py
@@ -37,7 +37,9 @@ for input_ in inputs:
 def get_loss(opt):
     # encode audio feature
     logits = get_logit(opt.input[opt.gpu_index], voca_size=voca_size)
+    print(logits)
     dense = get_predictions(logits)
+
     # CTC loss
     return dense.sg_ctc(target=opt.target[opt.gpu_index], seq_len=opt.seq_len[opt.gpu_index])
 
