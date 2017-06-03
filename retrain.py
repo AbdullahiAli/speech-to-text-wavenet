7967566d15bb31a972599777bbd88439c7033317
abdullahi <abdullahi154@gmail.com>
2017-06-03 13:54:03 +0200

    Strategy2

--- retrain.py
+++ retrain.py
@@ -46,7 +46,7 @@ def get_loss(opt):
 #
 # retrain
 #
-tf.sg_train(optim = 'Adam', lr=0.1, loss=get_loss(input=inputs, target=labels, seq_len=seq_len),
-            ep_size=non_native_data.num_batch, max_ep=50)
+tf.sg_train(optim = 'Adam', lr=0.01, loss=get_loss(input=inputs, target=labels, seq_len=seq_len),
+            ep_size=non_native_data.num_batch, max_ep=200)
             
 
